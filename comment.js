daa52b67a0264f7dca0f0f6c2efea32f271df1b2
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2024-02-11 19:31:37 -0600

    correction

--- comment.js
+++ comment.js
@@ -1,34 +1,44 @@
 // Create web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-
-var app = http.createServer(function(request, response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-    var description = queryData.id;
-    var list = template.list();
-    var html = template.HTML(title, list,
-        `
-        <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-                <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-                <input type="submit">
-            </p>
-        </form>
-        `,
-        ''
-    );
-    response.writeHead(200);
-    response.end(html);
-});
-app.listen(3000);
+// 1. Create a web server using the http module
+const server = http.createServer((req, res) => {
+  // 2. Create a route for the home page
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    });
+  }
+    // 3. Create a route for the about page
+    else if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+        }
+    // 4. Create a route for the contact page
+    else if (req.url === '/contact') {
+        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+        }
+    // 5. Create a route for the 404 page
+    else {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+    }
+}
+);
+// 6. Start the web server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
